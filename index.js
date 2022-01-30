36a5a6ee8dd0bf3e0d21f8f672bcbb32fd667443
Anush Suresh Kumar <sureshka@oregonstate.edu>
2022-01-29 21:25:04 -0800

    CS561ASK-45: Added check for bearer token for all the APIs

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 const app = express()
 const port = 3000
 const BASE_PATH = "/v1/"
-// Risk: Add to secrets/environment
 const TOKEN_SECRET = 'cd47d09523b2a3e304209f8aeb63c06c5d8ece4bf36d6df1bf72b2d98d080536'
 
 const corvallisWeather = {
@@ -54,42 +53,95 @@ const corvallisWeather = {
 }
 
 // for parsing application/json
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // for parsing application/xwww-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get(BASE_PATH + 'weather', (req, res) => {
-    res.setHeader("content-type", "application/json")
-    res.send(corvallisWeather)
+    let token = getBearerToken(req);
+    if (!token) {
+        res.status(400)
+        res.send({ "type": "error", "message": "Authorization Token Missing" })
+    } else if (!validateToken(token)) {
+        res.status(401)
+        res.send({ "type": "error", "message": "Invalid Authorization Token" })
+    } else {
+        res.status(200)
+        res.json(corvallisWeather)
+    }
 })
 
 app.get(BASE_PATH + 'hello', (req, res) => {
-    res.setHeader("content-type", "application/json")
-    res.send({"Hello" : "World!"})
+    let token = getBearerToken(req);
+    if (!token) {
+        res.status(400)
+        res.send({ "type": "error", "message": "Authorization Token Missing" })
+    } else if (!validateToken(token)) {
+        res.status(401)
+        res.send({ "type": "error", "message": "Invalid Authorization Token" })
+    } else {
+        res.status(200)
+        res.send({ "Hello": "World!" })
+    }
 })
 
 app.post(BASE_PATH + 'auth', (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
-    // All tokens have 60 mins validity
     var expiresIn = new Date(new Date().getTime() + 3600000).toJSON()
     res.setHeader("content-type", "application/json")
 
-    if(username == undefined || password == undefined || username.length == 0 || password.length == 0){
+    if (username == undefined || password == undefined || username.length == 0 || password.length == 0) {
         res.status(400)
         res.send({
             "type": "error",
             "message": "Invalid Input"
-          })
-    }else{
+        })
+    } else {
         res.send({
-            "access-token": jwt.sign({"username" : username, "expiry": expiresIn}, TOKEN_SECRET),
+            "access-token": jwt.sign({ "username": username, "expiry": expiresIn }, TOKEN_SECRET),
             "expires": expiresIn
-          }
+        }
         )
     }
 })
+
+/**
+ * This function is responsible to validate the auth token 
+ * @param {String} token 
+ * @returns boolean
+ * True: If the token is a valid JWT token
+ * False: If the token is invalid
+ * 
+ * Note: We are not checking for token expiry
+ */
+function validateToken(token) {
+    try {
+        jwt.verify(token, TOKEN_SECRET);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+/**
+ * This fucntion is responsible to extract the token from the HTTP headers 
+ * @param req 
+ * @returns 
+ *  undefined: If token is not found
+ *  string: Token
+ */
+function getBearerToken(req) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return undefined;
+    }
+    return token;
+}
+
 
 app.listen(port, () => {
     console.log(`API Mocker app listening at http://localhost:${port}`)
